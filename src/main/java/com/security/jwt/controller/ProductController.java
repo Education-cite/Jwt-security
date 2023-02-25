8fe487f2a0dbf202802c6db98487029a992e7042
Education-cite <tusarjobs@gmail.com>
2023-02-25 17:19:53 +0600

    e

--- src/main/java/com/security/jwt/controller/ProductController.java
+++ src/main/java/com/security/jwt/controller/ProductController.java
@@ -1,12 +1,22 @@
 package com.security.jwt.controller;
 
+import java.awt.PageAttributes;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.tomcat.util.http.parser.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.security.jwt.entity.ImageModel;
 import com.security.jwt.entity.Product;
 import com.security.jwt.service.ProductService;
 
@@ -17,13 +27,44 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 	
-	@PostMapping({"/addNewProduct"})
-	public Product addNewProduct(@RequestBody Product product) {
-		return productService.addNewProduct(product);
+	@PreAuthorize("hasRole('Admin')")
+	@PostMapping(value ={"/addNewProduct"},consumes= {org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE})
+
+	public Product addNewProduct(@RequestPart("product") Product product,@RequestPart("imageFile") MultipartFile[] file) {
+		//return productService.addNewProduct(product);
+		
+		try{
+			Set<ImageModel> images = uploadImage(file);
+			product.setProductimages(images);
+			return productService.addNewProduct(product);
+
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+			return null;
+		}
+		
+		
 	}
 	
 	
-	
+	public Set<ImageModel> uploadImage(MultipartFile[] multipartFiles) throws IOException{
+		Set<ImageModel> imageModels = new HashSet<>();
+		
+		for(MultipartFile file:multipartFiles){
+		ImageModel imageModel = new ImageModel(
+				file.getOriginalFilename(),
+				file.getContentType(),
+				file.getBytes()
+				
+				);
+		
+		imageModels.add(imageModel);
+		
+		}
+		
+		return imageModels;
+		
+	}
 	
 	
 
--- src/main/java/com/security/jwt/entity/Product.java
+++ src/main/java/com/security/jwt/entity/Product.java
@@ -1,9 +1,16 @@
 package com.security.jwt.entity;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Product {
@@ -16,6 +23,18 @@ public class Product {
 	private String productDescription;;
 	private Double productDiscountedPrice;
 	private Double productActualPrice;
+	
+	@ManyToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+	private Set<ImageModel> productimages;
+	@JoinTable(name="product_images",
+			joinColumns = {
+					@JoinColumn(name="product_id")
+			},
+			inverseJoinColumns = {
+					@JoinColumn(name="image_id")
+
+			}
+			)
 	
 	
 	public Integer getProductId() {
@@ -47,6 +66,12 @@ public class Product {
 	}
 	public void setProductActualPrice(Double producActualPrice) {
 		this.productActualPrice = producActualPrice;
+	}
+	public Set<ImageModel> getProductimages() {
+		return productimages;
+	}
+	public void setProductimages(Set<ImageModel> productimages) {
+		this.productimages = productimages;
 	}
 	
 	 
